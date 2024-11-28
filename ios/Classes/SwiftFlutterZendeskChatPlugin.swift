d091217b51b34435e6e2a35f0b36cfa3953a9ce8
VitalikShcherbyna <vitalik.shcherbyna@gmail.com>
2024-11-28 10:52:21 +0100

    f

--- ios/Classes/SwiftFlutterZendeskChatPlugin.swift
+++ ios/Classes/SwiftFlutterZendeskChatPlugin.swift
@@ -206,8 +206,8 @@ public class SwiftFlutterZendeskChatPlugin: NSObject, FlutterPlugin {
             if let myArgs = args as? [String: Any],
                 let message: String = myArgs["message"] as? String
             {
-                if let phoneN = Chat.instance?.configuration.visitorInfo.phoneNumber {
-                    print("Chat.instance?.configuration.visitorInfo \(phoneN)")
+                if let v = Chat.instance?.configuration.visitorInfo {
+                    print("Chat.instance?.configuration.visitorInfo \(v.phoneNumber)")
                 }
                 Chat.chatProvider?.sendOfflineForm(OfflineForm(visitorInfo: Chat.instance?.configuration.visitorInfo, departmentId: Chat.instance?.configuration.department, message: message)) { (outcome) in
                     switch outcome {
